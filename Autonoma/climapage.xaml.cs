e387a8544ed0645c0781457ed2d82380e6ead498
alum_tes_b <alum_tes_b@espiritusanto.ec>
2023-12-02 13:39:36 -0500

    segundo commit

--- Autonoma/climapage.xaml.cs
+++ Autonoma/climapage.xaml.cs
@@ -1,3 +1,5 @@
+using Autonoma.Service;
+
 namespace Autonoma;
 
 public partial class climapage : ContentPage
@@ -6,4 +8,18 @@ public partial class climapage : ContentPage
 	{
 		InitializeComponent();
 	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var result = await (AppService.GetClima(40.4170, -3.7022));
+        LblCiudad.Text = result.locality.name;
+        LblClima.Text = result.hour_hour.hour1.text;
+        LblPais.Text = result.locality.country;
+        LblTemperatura.Text = result.hour_hour.hour1.temperature +
+        result.information.temperature;
+        LblHumedad.Text = result.day1.humidity + result.information.humidity;
+        LblViento.Text = result.day1.wind + result.information.wind;
+        ImgHumedadIcon.Source = "humedadicon.png";
+        ImgVientoIcon.Source = "vientoicon.png";
+    }
 }
